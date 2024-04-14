4191d5d696b3147fea42efc91b2001352da8fd45
Jack Mitchell <jackmitc@usc.edu>
2024-04-14 03:39:29 +0000

    first pass

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,25 +21,66 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool schedHelper(
+  const AvailabilityMatrix& avail,
+  const size_t n,
+  const size_t d,
+  const size_t m,
+  const size_t k,
+  const size_t i,
+  DailySchedule& sched,
+  std::map<Worker_T, int>& shiftmap
+);
 
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
-    const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
-    DailySchedule& sched
+  const AvailabilityMatrix& avail,
+  const size_t dailyNeed,
+  const size_t maxShifts,
+  DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
-        return false;
+  std::map<Worker_T, int> shiftmap;
+  if(avail.size() == 0U){
+    return false;
+  }
+  sched.clear();
+  // Add your code below
+  int n = avail.size(), d = dailyNeed, m = maxShifts, k = avail[0].size();
+  for(int i = 0; i < n; i++){ // initialize "blank" sched (completely invalid)
+    vector<Worker_T> v;
+    sched.push_back(v);
+    for(int j = 0; j < d; j++){
+      sched[i].push_back(INVALID_ID);
     }
-    sched.clear();
-    // Add your code below
-
-
-
-
+  }
+  return schedHelper(avail, n, d, m, k, 0, sched, shiftmap); // kick off recursion
 }
 
+bool schedHelper(
+  const AvailabilityMatrix& avail,
+  const size_t n,
+  const size_t d,
+  const size_t m,
+  const size_t k,
+  const size_t i,
+  DailySchedule& sched,
+  std::map<Worker_T, int>& shiftmap
+){
+  if(i == n * d) return true; // reached the end!
+  if(sched[i / d][i % d] == INVALID_ID){
+    for(int w = 0; w < k; w++){ // cycle through options (each worker)
+      sched[i / d][i % d] = w; // 1d to 2d
+      if((shiftmap[w] + 1 <= m) and (avail[i / d][w] == 1)){ // check these before making the changes to save a line or two
+        shiftmap[w]++;
+        if(schedHelper(avail, n, d, m, k, i + 1, sched, shiftmap)) return true; // recurse down sched
+      }
+      sched[i / d][i % d] = INVALID_ID; // backtrack if invalid
+    }
+  } else{
+    std::cout << "this shouldn't print" << endl; // as you might have guessed, this shouldn't print
+    return schedHelper(avail, n, d, m, k, i + 1, sched, shiftmap);
+  }
+  return false; // made it through all the options and none were valid
+}
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include<iterator>
 #endif
 
 #include "wordle.h"
@@ -13,6 +14,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+int count(const std::string&, const char&);
+void gencombs(std::string, std::string, std::set<std::string>&);
+void alpha(std::string, std::set<std::string>&, int, int);
 
 
 // Definition of primary wordle function
@@ -21,8 +25,49 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+  // Add your code here
+  std::set<std::string> retset, temp;
+  gencombs(floating, in, temp); // recursive helper function to generate all combinations
+  std::set<std::string>::iterator it = temp.begin();
+  for(; it != temp.end(); ++it){ // transfer every dictionary-verified word into return set
+    if(dict.find(*it) != dict.end()) retset.insert(*it);
+  }
+  return retset;
 }
 
 // Define any helper functions here
+int count(const std::string& s, const char& c){
+  int i = 0, count = (s[0] == c) - 1; // loop guaranteed to run once, so subtract one to compensate
+  while(i != std::string::npos){
+    i = s.find(c, i + 1); // update index to 1 past next occurrence of the character
+    count++;
+  }
+  return count;
+}
+
+void gencombs(std::string letterstoiterate, std::string curr, std::set<std::string>& combs){
+  if(!count(curr, '-')) combs.insert(curr); // word complete
+  else{
+    for(int i = 0; i < curr.size(); i++){ // iterate through "--x-"
+      if(curr[i] == '-'){ // need to recurse down this path
+        if(letterstoiterate.size()){ // if there are still floating letters to use
+          for(int j = 0; j < letterstoiterate.size(); j++){ // iterate over floaters
+            string newlets = letterstoiterate;
+            newlets.erase(newlets.begin() + j); // remove replacing floater from floating list
+            string temp = curr;
+            temp[i] = letterstoiterate[j]; // replace '-' with floater
+            gencombs(newlets, temp, combs); // recurse with '-' replaced and the replacing floater removed
+          }
+        } else alpha(curr, combs, i, 0); // no floating letters to use, so cycle a-z
+      }
+    }
+  }
+}
+
+void alpha(std::string curr, std::set<std::string>& s, int index, int offset){
+  if(offset == 26) return; // 'z' done in last call
+  string temp = curr;
+  temp[index] = 'a' + offset; // replace the '-' with a letter a-z
+  gencombs("", temp, s); // generate combinations with the current setup (no floating left)
+  alpha(curr, s, index, offset + 1); // cycle a-z
+}
